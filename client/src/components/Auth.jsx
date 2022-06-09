bb9934247f5ab0222e3543a126cd9bc8ffc99e15
escalandres <aescalaa1900@alumno.ipn.mx>
2022-06-08 20:18:45 -0500

    Comentarios eliminados

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -53,7 +53,7 @@ const Auth = () => {
             const { username, password, phoneNumber, avatarURL } = form;
 
             //const URL = 'http://localhost:5000/auth';
-            const URL = 'https://chat-app-project-ing-web.com/auth';
+            const URL = 'https://chat-app-project-ing-web.herokuapp.com/auth';
 
             const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
                 username, password, fullName: form.fullName, phoneNumber, avatarURL,
